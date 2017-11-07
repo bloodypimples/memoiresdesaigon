024e79056d9726296c673aea69cf42315542db8c
Hieu <hieudoan2609@gmail.com>
2017-11-07 19:53:48 +0700

    fix a heroku bug #2

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,7 +5,9 @@ class RoomsController < ApplicationController
   def show
     @room = Room.find(params[:id])
     @rooms = Room.all.order("created_at desc")
-    @gallery = @room.gallery
-    @gallery_images = @gallery.gallery_images
+    if @room.gallery
+      @gallery = @room.gallery
+      @gallery_images = @gallery.gallery_images
+    end
   end
 end
