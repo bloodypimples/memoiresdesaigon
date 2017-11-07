12a6bb8d07429a0a0dba098a04c8800536904727
Hieu <hieudoan2609@gmail.com>
2017-11-07 20:23:05 +0700

    add hero size images

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
-  has_attached_file :image, styles: { medium: "600x600>", thumb: "100x100>" }, default_url: "/assets/placeholder.png"
+  has_attached_file :image, styles: { hero: "1300x1300>", medium: "600x600>", thumb: "100x100>" }, default_url: "/assets/placeholder.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   has_one :gallery, dependent: :destroy
 end
