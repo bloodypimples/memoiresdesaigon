8a3dfa4bdd562adc1d1f1b9874f18585a16cad23
Hieu <hieudoan2609@gmail.com>
2017-11-12 11:25:42 +0700

    change email provider to SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,11 +69,11 @@ Rails.application.configure do
   host = 'memoiresdesaigon.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.gmail.com',
+    :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['GMAIL_USERNAME'],
-    :password       => ENV['GMAIL_PASSWORD'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
